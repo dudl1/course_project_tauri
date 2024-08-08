a0883007e521c4ee0f2484bd9804924fda507bdb
Valery <80582147+dudl1@users.noreply.github.com>
2024-08-08 23:58:10 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+async function loadTemplate(filePath)
+{
+    try
+    {
+        const response = await fetch(filePath);
+        if (!response.ok) throw new Error('Ошибка загрузки шаблона');
+
+        const templateText = await response.text();
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = templateText;
+
+        const externalTemplate = tempDiv.querySelector('template');
+        if (externalTemplate)
+        {
+            return externalTemplate.content.cloneNode(true);
+        }
+        else
+        {
+            throw new Error('Шаблон не найден');
+        }
+    }
+    catch (error)
+    {
+        console.error('Ошибка загрузки шаблона:', error);
+        return null;
+    }
+}
+
+async function loadCSS()
+{
+    try
+    {
+        const response = await fetch('/style.css');
+        if (!response.ok) throw new Error('Ошибка загрузки CSS');
+
+        const cssText = await response.text();
+        const style = document.createElement('style');
+        style.textContent = cssText;
+        document.head.appendChild(style);
+    }
+    catch (error)
+    {
+        console.error('Ошибка загрузки CSS:', error);
+    }
+}
+
+async function loadExternalTemplates()
+{
+    await loadCSS();
+
+    const templates = [
+        './components/window_menu.html',
+        './components/work.html'
+    ];
+
+    const contentDiv = document.getElementById('app');
+    for (const template of templates)
+    {
+        const templateContent = await loadTemplate(template);
+        if (templateContent)
+        {
+            contentDiv.appendChild(templateContent);
+        }
+    }
+
+    const scripts = contentDiv.querySelectorAll('script');
+    scripts.forEach(
+        script =>
+        {
+            const scriptText = script.innerHTML.trim();
+            if (scriptText)
+            {
+                const fn = new Function('document', scriptText);
+                fn(document);
+            }
+        }
+    );
+}
+loadExternalTemplates();
---
+++ scripts/file_reader.js
@@ -0,0 +1,88 @@
+let isFileAnylogic = false;
+
+function func_file_anylogic()
+{
+    let windowAnylogic = document.querySelector(".window_anylogic");
+    windowAnylogic.classList.add("active");
+
+    let iframe = document.getElementById("anylogic-frame");
+    iframe.src = "https://cloud.anylogic.com/models";
+
+    document.querySelector(".window_anylogic").classList.add("active");
+    if (!document.querySelector(".not_work").classList.contains("active"))
+        document.querySelector(".not_work").classList.add("active");
+}
+
+
+let isFileExcel = false;
+
+function func_file_excel(file_excel)
+{
+    if (!file_excel) return;
+
+    file_excel.click();
+
+    if (!isFileExcel)
+    {
+        file_excel.addEventListener('change', function(event)
+        {
+            const file = event.target.files[0];
+            
+            if (file)
+            {
+                var reader = new FileReader();
+                reader.onload = function(e)
+                {
+                    var data = new Uint8Array(e.target.result);
+                    var workbook = XLSX.read(data, { type: "array" });
+                    
+                    var worksheet = workbook.Sheets[workbook.SheetNames[0]];
+                    var jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+                    
+                    function parseExcelData(rawData)
+                    {
+                        if (rawData.length < 2)
+                        {
+                            console.error("Недостаточно данных для обработки.");
+                            return [];
+                        }
+                    
+                        var headers = rawData[0];
+                        var dataRows = rawData.slice(1);
+                    
+                        function createObjectFromRow(row, index)
+                        {
+                            var obj = { id: index + 1 };
+                            headers.forEach((header, idx)=>
+                            {
+                                obj[header.toLowerCase().replace(/[^a-z0-9]/gi, '_')] = row[idx] || '';
+                            });
+                            return obj;
+                        }
+                    
+                        var tabledata = dataRows
+                            .filter(row => row.some(cell => cell))
+                            .map((row, index) => createObjectFromRow(row, index));
+                    
+                        return tabledata;
+                    }
+
+                    var tabledata = parseExcelData(jsonData);
+            
+                    new Tabulator("#excel_content", {
+                        data: tabledata,
+                        autoColumns: true,
+                    });
+                };
+                reader.readAsArrayBuffer(file);
+
+                document.querySelector(".window_excel").classList.add("active");
+                if (!document.querySelector(".not_work").classList.contains("active"))
+                    document.querySelector(".not_work").classList.add("active");
+            }
+        });
+
+        isFileExcel = true;
+    }
+}
